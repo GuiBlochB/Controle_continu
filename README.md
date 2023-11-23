7a8613b30b967da1db6b5a1d65e994b3ed54483f
guillaume <guillaume.blochbaraux@gmail.com>
2023-11-23 14:07:02 +0100

    Adding main.py with program

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Le contrôle continu de GEMA Git
 
 Ce Projet a pour de résoudre de répondre à des questions posées par l'intervenante lors du cours de Git.
+
+Un petit projet de débutant en Python pour utiliser input et while.
+Il faut deviner le nombre choisi au hasard par l'ordinateur
---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+
+keep_going = True
+
+while keep_going:
+    number_picked = random.randrange(0, 11)
+    guessed = input("Trouver un nombre entre 1 et 10 : ")
+    if guessed != number_picked:
+        print(f"Perdu ! C'était {number_picked}")
+        keep_going = True if input("On rejoue ? Oui : Non ").lower() == "oui" else False
+    else:
+        print("C'est gagné !")
+        keep_going = True if input("On rejoue ? Oui : Non ").lower() == "oui" else False
+print("Bye !")
